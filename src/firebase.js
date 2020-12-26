251ac513f0299bd24442b2332aa09c06913c734a
Endercan Yılmaz <55192890+Ender142@users.noreply.github.com>
2020-12-26 15:15:33 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCsSdJ8ENAc0I29QmbKUHQRJ_UAAW0jef8",
+    apiKey: "***********c0I29QmbKUHQRJ_UAAW0jef8",
     authDomain: "reacttodo-c4ad9.firebaseapp.com",
     projectId: "reacttodo-c4ad9",
     storageBucket: "reacttodo-c4ad9.appspot.com",
-    messagingSenderId: "306885833858",
-    appId: "1:306885833858:web:3905c6f493730ba947b09b", 
+    messagingSenderId: "***885833858",
+    appId: "1:306****33858:web:3905c6f493730ba947b09b", 
     measurementId: "G-0F3QJZ412D"
 });
 
